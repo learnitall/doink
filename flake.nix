b1365be0232026f19564d9b2dae452e8e3e2069a
Ryan Drew <learnitall0@gmail.com>
2024-05-26 15:51:02 +0000

    Add gitignore and flake.nix
    Signed-off-by: Ryan Drew <learnitall0@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Distributed Objects In Kafka";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.devshell.url = "github:numtide/devshell";
+
+  outputs = { nixpkgs, flake-utils, devshell, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+	  inherit system;
+	  overlays = [
+	    devshell.overlays.default
+	  ];
+	};
+	packages = with pkgs; [
+	  poetry
+	  python312
+	];
+      in
+      {
+        devShells.default = pkgs.devshell.mkShell rec {
+	  name = "doink";
+	  inherit packages;
+	  devshell.startup."setprompt" = pkgs.lib.noDepEntry ''
+	    export LP_MARK_PREFIX=" (doink) "
+	  '';
+	  devshell.startup."printpackages" = pkgs.lib.noDepEntry ''
+	    echo "[[ Packages ]]"
+	    echo "${builtins.concatStringsSep "\n" (builtins.map (p: p.name) packages)}"
+	    echo ""
+	  '';
+	};
+      }
+    );
+}
